ecd2e433ce62b0e64b31153ce20a3558dcf6e9ec
user2 <user2@KH_I>
2019-11-14 21:19:29 +0900

    doo pageInfo수정

--- arp03/src/main/java/com/kh/arp/common/PageInfo.java
+++ arp03/src/main/java/com/kh/arp/common/PageInfo.java
@@ -1,29 +1,30 @@
 package com.kh.arp.common;
 
 public class PageInfo {
-	
+
 	private int currentPage;
 	private int listCount;
 	private int pageLimit;
+	private int boardLimit;
 	private int maxPage;
 	private int startPage;
 	private int endPage;
-	private int boardLimit;
-	
-	public PageInfo() {
-		
-	}
-
-	public PageInfo(int currentPage, int listCount, int pageLimit, int maxPage, int startPage, int endPage,
-			int boardLimit) {
-		super();
-		this.currentPage = currentPage;
-		this.listCount = listCount;
+	private int startRow;
+	private int endRow;
+	private int offset;
+	   
+	public PageInfo(int currentPage,int listCount, int pageLimit, int boardLimit) {
+		this.currentPage=currentPage;
+		this.listCount=listCount;
 		this.pageLimit = pageLimit;
-		this.maxPage = maxPage;
-		this.startPage = startPage;
-		this.endPage = endPage;
 		this.boardLimit = boardLimit;
+		      
+		maxPage=(int)Math.ceil((double)listCount/boardLimit);
+		startPage=((currentPage-1)/pageLimit)*pageLimit+1;
+		endPage=Math.min(startPage+pageLimit-1,maxPage);
+		startRow=(currentPage-1)*boardLimit+1;
+		endRow=currentPage*boardLimit;
+		offset=startRow-1;
 	}
 
 	public int getCurrentPage() {
@@ -50,6 +51,14 @@ public class PageInfo {
 		this.pageLimit = pageLimit;
 	}
 
+	public int getBoardLimit() {
+		return boardLimit;
+	}
+
+	public void setBoardLimit(int boardLimit) {
+		this.boardLimit = boardLimit;
+	}
+
 	public int getMaxPage() {
 		return maxPage;
 	}
@@ -74,19 +83,35 @@ public class PageInfo {
 		this.endPage = endPage;
 	}
 
-	public int getBoardLimit() {
-		return boardLimit;
+	public int getStartRow() {
+		return startRow;
 	}
 
-	public void setBoardLimit(int boardLimit) {
-		this.boardLimit = boardLimit;
+	public void setStartRow(int startRow) {
+		this.startRow = startRow;
+	}
+
+	public int getEndRow() {
+		return endRow;
+	}
+
+	public void setEndRow(int endRow) {
+		this.endRow = endRow;
+	}
+
+	public int getOffset() {
+		return offset;
+	}
+
+	public void setOffset(int offset) {
+		this.offset = offset;
 	}
 
 	@Override
 	public String toString() {
 		return "PageInfo [currentPage=" + currentPage + ", listCount=" + listCount + ", pageLimit=" + pageLimit
-				+ ", maxPage=" + maxPage + ", startPage=" + startPage + ", endPage=" + endPage + ", boardLimit="
-				+ boardLimit + "]";
+				+ ", boardLimit=" + boardLimit + ", maxPage=" + maxPage + ", startPage=" + startPage + ", endPage="
+				+ endPage + ", startRow=" + startRow + ", endRow=" + endRow + ", offset=" + offset + "]";
 	}
 	
 	
