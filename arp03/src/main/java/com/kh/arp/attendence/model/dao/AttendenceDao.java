8031c21c6c10cc3ab9589c4e96ca5c628542536b
minsang.kim <minsang.kim@DESKTOP-AG479K7>
2019-11-15 20:25:21 +0900

    수정

--- arp03/src/main/java/com/kh/arp/attendence/model/dao/AttendenceDao.java
+++ arp03/src/main/java/com/kh/arp/attendence/model/dao/AttendenceDao.java
@@ -2,9 +2,11 @@ package com.kh.arp.attendence.model.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.kh.arp.attendence.model.vo.Attendence;
 
+@Repository
 public class AttendenceDao {
 
 	@Autowired
--- arp03/src/main/java/com/kh/arp/attendence/model/service/AttendenceServiceImpl.java
+++ arp03/src/main/java/com/kh/arp/attendence/model/service/AttendenceServiceImpl.java
@@ -1,19 +1,12 @@
 package com.kh.arp.attendence.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-<<<<<<< HEAD
 import org.springframework.stereotype.Service;
 
 import com.kh.arp.attendence.model.dao.AttendenceDao;
 import com.kh.arp.attendence.model.vo.Attendence;
 
 @Service
-=======
-
-import com.kh.arp.attendence.model.dao.AttendenceDao;
-import com.kh.arp.attendence.model.vo.Attendence;
-
->>>>>>> branch 'dev' of https://github.com/dooyounglee/arp03
 public class AttendenceServiceImpl implements AttendenceService {
 		
 	@Autowired
@@ -21,11 +14,7 @@ public class AttendenceServiceImpl implements AttendenceService {
 	
 	@Override
 	public Attendence selectStudentAttendence() {
-<<<<<<< HEAD
 		Attendence at = atDao.selectStudentAttendence();
-=======
-		Attendence at = atDao.selectStudentAttendence(); 
->>>>>>> branch 'dev' of https://github.com/dooyounglee/arp03
 		return null;
 	}
 
