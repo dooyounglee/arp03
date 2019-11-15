59c54e162fb588830bd2b7fee6a2d49dc25ccd69
minsang.kim <minsang.kim@DESKTOP-AG479K7>
2019-11-15 17:17:04 +0900

    vacation 수정

--- arp03/src/main/java/com/kh/arp/vacation/controller/VacationController.java
+++ arp03/src/main/java/com/kh/arp/vacation/controller/VacationController.java
@@ -1,5 +1,13 @@
 package com.kh.arp.vacation.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class VacationController {
 
+	
+	@RequestMapping("vList.me")
+	public String vacationListForm() {
+		
+		return "vacation/vacationListForm";
+	}
 }
