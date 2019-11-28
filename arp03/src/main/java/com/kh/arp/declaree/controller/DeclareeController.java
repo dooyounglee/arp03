cf1d8945d6bf1703ac779ca66b4798eee72e1570
yryr92 <rin217@naver.com>
2019-11-28 18:01:29 +0900

    yr push

--- arp03/src/main/java/com/kh/arp/declaree/controller/DeclareeController.java
+++ arp03/src/main/java/com/kh/arp/declaree/controller/DeclareeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.arp.board.model.service.BoardService;
@@ -21,22 +22,34 @@ public class DeclareeController {
 
 	@Autowired
 	private DeclareeService ds;
-	
+
 	@PostMapping("insertForm.de")
 	public ModelAndView declareGet(ModelAndView mv, String obj, int obj_no) {
-		//System.out.println(obj_no);
+		// System.out.println(obj_no);
 		mv.addObject("obj", obj).addObject("obj_no", obj_no).setViewName("declare/declare");
 		return mv;
 	}
+
 	
-	@PostMapping("insert.de")
-	   public ModelAndView declarePost(Declaree d, HttpSession session, ModelAndView mv) {
-	      Member m=(Member)session.getAttribute("mem");
-	      d.setM_no(m.getM_no());
-	      //System.out.println(d);
-	      int result=ds.declare(d);
-	      mv.setViewName("declare/declare");
-	      return mv;
-	   }
+	 @PostMapping("insert.de") 
+	 public ModelAndView declarePost(Declaree d,HttpSession session, ModelAndView mv) { 
+		 Member m=(Member)session.getAttribute("mem"); 
+		 d.setM_no(m.getM_no());
+		 System.out.println(d); 
+		 int result=ds.declare(d);
+		 mv.setViewName("declare/declare"); 
+		 return mv; 
+	 }
+	 
+	/*
+	 * @ResponseBody
+	 * 
+	 * @PostMapping("insert.de") public String declarePost(Declaree d, HttpSession
+	 * session) { Member m=(Member)session.getAttribute("mem");
+	 * d.setM_no(m.getM_no()); //System.out.println(d); int result=ds.declare(d);
+	 * if(result > 0) { return "success"; } else { return "fail"; }
+	 * 
+	 * }
+	 */
 
 }
