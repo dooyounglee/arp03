5c603c94f6e5e899e00101c69760579cc4d85e0c
user2 <user2@KH_I>
2019-12-04 17:52:50 +0900

    # WARNING: head commit changed in the meantime
    doo 이제 에러페이지 안뜸. 콘솔창 가서 확인해야함

--- arp03/src/main/java/com/kh/arp/MainController.java
+++ arp03/src/main/java/com/kh/arp/MainController.java
@@ -1,14 +1,30 @@
 package com.kh.arp;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.arp.board.model.vo.Board;
+import com.kh.arp.member.model.service.MemberService;
+import com.kh.arp.notice.model.vo.Notice;
 
 @Controller
 public class MainController {
 
+	@Autowired
+	private MemberService ms;
+	
 	@RequestMapping("/")
-	public String main() {
-		return "index";
+	public ModelAndView main(ModelAndView mv) {
+		List<Board> blist=ms.getBoardListForMain();
+		List<Notice> nlist=ms.getNoticeListForMain();
+		mv.addObject("blist", blist);
+		mv.addObject("nlist", nlist);
+		mv.setViewName("index");
+		return mv;
 	}
 	
 	
